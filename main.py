75152e4f5a927dc664d443cb509dcd8b52ba09af
oyatillouzbek <63519552+oyatillouzbek@users.noreply.github.com>
2020-04-22 06:10:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ password = "7184838441baf33aa0986afeca61e726ab610163a77c357087e3e826fc71fc5c"
 hostname = "ec2-54-210-128-153.compute-1.amazonaws.com"
 database = "d7tofl99vg7pq2"
 
-connection = psycopg2.connect(username,
-    password,hostname,database)
+connection = psycopg2.connect(username,password,hostname,database)
 
 bot = telebot.TeleBot("931190511:AAEuhHmrIiN5Lc_lNQq-ANjeauytWH2i5Gc")
 restricted_messages = ["zzz", "zver"]
