b93209a4b9643209f441ac38174d347ff9052f89
oyatillouzbek <63519552+oyatillouzbek@users.noreply.github.com>
2020-04-22 05:37:09 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import psycopg2
+from psycopg2 import Error
+
+try:
+    connection = psycopg2.connect(user = "thzrixmbpxycue",
+                                  password = "7184838441baf33aa0986afeca61e726ab610163a77c357087e3e826fc71fc5c",
+                                  host = "ec2-54-210-128-153.compute-1.amazonaws.com",
+                                  database = "d7tofl99vg7pq2")
+
+    cursor = connection.cursor()
+    
+    create_table_query = '''CREATE TABLE mobile
+          (ID INT PRIMARY KEY     NOT NULL,
+          MODEL           TEXT    NOT NULL,
+          PRICE         REAL); '''
+    
+    cursor.execute(create_table_query)
+    connection.commit()
+    print("Table created successfully in PostgreSQL ")
+
+except (Exception, psycopg2.DatabaseError) as error :
+    print ("Error while creating PostgreSQL table", error)
+finally:
+    #closing database connection.
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
