5b39e3b5ef8c5e10fbd97524989c0e62110acfd6
oyatillouzbek <63519552+oyatillouzbek@users.noreply.github.com>
2020-04-23 08:34:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,30 @@ import telebot
 import time
 from telebot import types
 import psycopg2
+def newuser(message):
+    try:
+        connection = psycopg2.connect(user = "thzrixmbpxycue",
+                                  password = "7184838441baf33aa0986afeca61e726ab610163a77c357087e3e826fc71fc5c",
+                                  host = "ec2-54-210-128-153.compute-1.amazonaws.com",
+                                  database = "d7tofl99vg7pq2")
+        cursor = connection.cursor()
+        sql_select_query = """select * from grs"""
+        cursor.execute(sql_select_query)
+        record = cursor.fetchall()
+        if record not in message.from_user.id:
+            sql_update_query = """INSERT INTO grs (grid, userid, kanal) VALUES (%s, %s, %s)"""
+            cursor.execute(sql_update_query, (message.chat.id,message.from_user.id,''))
+            
+            bot.send_message(message.chat.id, "Bazaga yozildi")
+        else:
+            bot.send_message(message.chat.id, "avvaldan borsiz")
+        sql_select_query = """select * from grs"""
+        cursor.execute(sql_select_query)
+        record = cursor.fetchall()
+        connection.commit()
+        
+    except (Exception, psycopg2.Error) as error:
+        print("Error in update operation", error)
 
 username = "thzrixmbpxycue"
 password = "7184838441baf33aa0986afeca61e726ab610163a77c357087e3e826fc71fc5c"
@@ -17,24 +41,22 @@ bot = telebot.TeleBot("931190511:AAEuhHmrIiN5Lc_lNQq-ANjeauytWH2i5Gc")
 restricted_messages = ["zzz", "zver"]
 
 @bot.message_handler(commands=['add'])
+
 def adddata(message):
-    with connection.cursor() as cursor:
-        sql = "INSERT INTO grs (grid, userid, kanal) VALUES (%s, %s, %s)"
-        cursor.execute(sql, (message.chat.id,message.from_user.id,''))
-        bot.send_message(message.chat.id, "Bazaga yozildi")
+    newuser(message)
 
 @bot.message_handler(commands=['getdata'])
 def getdata(message):
-        msg = ""
-        cursorr = connection.cursor()
-        sql = "SELECT * FROM grs"
-        cursorr.execute(sql)
-        resultt = cursorr.fetchall()
-        if resultt is None:
-            bot.send_message(message.chat.id, "hech narsa yoq")
-        else:
-            for x in resultt:
-                msg += "{}\n".format(x[0])
+    msg = ""
+    cursorr = connection.cursor()
+    sql = "SELECT * FROM grs"
+    cursorr.execute(sql)
+    resultt = cursorr.fetchall()
+    if resultt is None:
+        bot.send_message(message.chat.id, "hech narsa yoq")
+    else:
+        for x in resultt:
+            msg += "{}\n".format(x[0])
         bot.send_message(message.chat.id, msg)
 
 @bot.inline_handler(lambda query: query.query == "Telegram")
